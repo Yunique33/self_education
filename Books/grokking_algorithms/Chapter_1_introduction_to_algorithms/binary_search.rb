bc14ea455554ea2ec33fbeb5c4e60673329555ef
Yunus Ganiyev <yunique33@gmail.com>
2024-01-28 17:33:44 +0300

    add grokking algorithms book

---
+++ Books/grokking_algorithms/Chapter_1_introduction_to_algorithms/binary_search.rb
@@ -0,0 +1,22 @@
+def binary_search(list, item)
+  low = 0
+  high = list.length - 1
+  while low <= high
+    mid = (low + high) / 2
+    guess = list[mid]
+    if guess == item
+      mid
+    elsif guess > item
+      high = mid - 1
+    else
+      low = mid + 1
+    end
+  end
+  nil
+end
+
+my_list = [1, 3, 5, 7, 9]
+puts binary_search(my_list, 3) # => 1
+puts binary_search(my_list, -1) # => nil
+puts binary_search(my_list, 9) # => 4
+
---
+++ Books/grokking_algorithms/README.md
@@ -0,0 +1,4 @@
+# Grokking Algorithms
+
+Here are my solutions in Ruby to the exercises from Aditya Y. Bhargava's book, 
+[Grokking Algorithms](https://www.manning.com/books/grokking-algorithms).
