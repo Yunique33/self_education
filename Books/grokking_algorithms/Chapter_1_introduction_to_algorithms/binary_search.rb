ee5363bdcd6295a161b96d9c49dbb03523290311
Yunus Ganiyev <yunique33@gmail.com>
2024-01-28 17:35:17 +0300

    add grokking algorithms chapter 1

--- Books/grokking_algorithms/Chapter_1_introduction_to_algorithms/binary_search.rb
+++ Books/grokking_algorithms/Chapter_1_introduction_to_algorithms/binary_search.rb
@@ -1,17 +1,20 @@
 def binary_search(list, item)
   low = 0
   high = list.length - 1
+
   while low <= high
     mid = (low + high) / 2
     guess = list[mid]
+
     if guess == item
-      mid
+      return mid
     elsif guess > item
       high = mid - 1
     else
       low = mid + 1
     end
   end
+
   nil
 end
 
@@ -19,4 +22,3 @@ my_list = [1, 3, 5, 7, 9]
 puts binary_search(my_list, 3) # => 1
 puts binary_search(my_list, -1) # => nil
 puts binary_search(my_list, 9) # => 4
-
