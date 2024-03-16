ddce030fe15e47e32857d02548471ffb56acf3c5
Yunus Ganiyev <yunique33@gmail.com>
2024-03-16 21:59:11 +0300

    chapter 2 exercises draft

--- Books/grokking_algorithms/Chapter_2_selection_sort/selection_sort.rb
+++ Books/grokking_algorithms/Chapter_2_selection_sort/selection_sort.rb
@@ -16,18 +16,18 @@ puts find_smallest([10, 9, 8, -7, 6, 5, 4, 3, 2, 1]) # 3
 puts find_smallest([1, 10, 2, 9, 3, 8, 4, 7, 5, 6, 0]) # 10
 
 
-def selection_sort(arr)
-  result = []
-  arr.length.times do
-    smallest = find_smallest(arr)
-    binding.pry
-    result << smallest
-    arr.delete_at(smallest)
-  end
-  result.inspect
-end
-
-
-puts selection_sort([5, 3, 6, 2, 10]) # [2, 3, 5, 6, 10]
+# def selection_sort(arr)
+#   result = []
+#   arr.length.times do
+#     smallest = find_smallest(arr)
+#     binding.pry
+#     result << smallest
+#     arr.delete_at(smallest)
+#   end
+#   result.inspect
+# end
+#
+#
+# puts selection_sort([5, 3, 6, 2, 10]) # [2, 3, 5, 6, 10]
 # puts selection_sort([10, 9, 8, 7, 6, 5, 4, 3, 2, 1]) # [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 # puts selection_sort([1, 10, 2, 9, 3, 8, 4, 7, 5, 6, 0]) # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
