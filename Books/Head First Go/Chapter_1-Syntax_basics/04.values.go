670105e3cd43eedbf6796fee65de7462a8c58b17
Yunus Ganiyev <yunique33@gmail.com>
2025-12-29 00:39:31 +0300

    add chapter 1 of head first go

---
+++ Books/Head First Go/Chapter_1-Syntax_basics/04.values.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, Go!")
+	fmt.Println("Hello, \nGo!")
+	fmt.Println("Hello, \tGo!")
+	fmt.Println("Quotes: \"\"")
+	fmt.Println("Backslash: \\")
+
+	fmt.Println('A')
+	fmt.Println('B')
+	fmt.Println('Җ')
+	fmt.Println('\t')
+	fmt.Println('\n')
+	fmt.Println('\\')
+
+	fmt.Println(true)
+	fmt.Println(false)
+
+	fmt.Println(string(118))
+}
